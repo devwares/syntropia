b962a8900ae52ddab0d4fd50d267b051373c2052
dev@ntx <dev@wares.fr>
2017-06-30 17:43:14 +0200

    COMMIT DU WEEKEND - modifie telecommande.php et telecommande.js - ajout neongarage.php (pas fonctionnel, prevu pour etre inclu dans telcommande.php)

--- js/telecommande.js
+++ js/telecommande.js
@@ -1,20 +1,30 @@
-
 $(document).ready(function()
-
 		{
-
-			/* Ecouteur bouton neon garage ON */
+	
+			/* Code à implémenter */
+			$("#implement").html("echo '$.post(\'testpost.php\', {gpio_post_request: \'true\', neon-garage_state: \'high\'});'");
+			
+			/* Instructions Témoin */
+			$("#zonetest").html("Cette ligne apparait si le javascript fonctionne");
+			$("#zonetoto").hide(1500);
+			
+			/* Ecouteurs */
+			
 			$("#NeonGarageOn").on('click', function(even) {
-
-					$.post( 'simple-telecommande.php', {gpio_post_request: 'true', neon_garage_state: 'low' } );
-
+				
+				$('#zonetest').load('testload.htm', function() {
+					alert ("Mise à jour de la zone par bouton Low");
+					
+				});
+				
+			});
+			
+			$("#NeonGarageOff").on('click', function(even) {
+				
+				$.post('simple-telecommande.php',{ gpio_post_request:true, neonGarage_state: 'high'});
+				/* $.post('testpost.php',{ id:50, nom: 'durand'}); */
+				
 			});
 
-			/* Ecouteur bouton neon garage OFF */
-			$("#NeonGarageOff").on('click', function(even) {
-
-					$.post( 'simple-telecommande.php', {gpio_post_request: 'true', neon_garage_state: 'high' } );
-
-			});			
 
 		});
