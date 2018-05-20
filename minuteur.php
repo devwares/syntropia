cced69f4a86a7c5b2b12de899839d26d81232786
root <dev@wares.fr>
2018-05-21 00:46:11 +0200

    AJOUT MINUTEUR.PHP

---
+++ minuteur.php
@@ -0,0 +1,110 @@
+<?php
+namespace syntropia;
+include_once 'gpio.php';
+
+//$commande = 'echo $state='.$state.' >> /tmp/toto.txt';
+//exec($commande);
+
+class Minuteur extends Gpio
+{
+
+	/******************************************************************************
+	 * toString : redéfini pour afficher un bouton en jQuery
+	 ******************************************************************************/
+	public function __toString()
+	{
+		
+		try
+		{
+			$name = $this->_name;
+			$iddiv = 'div' . $name;
+			$idimg = 'img' . $name;
+			$pin = $this->_pin;
+			$label = $this->_label;
+			$state = $this->_gpioaccess->getValue();
+			
+			// Gpio High = Interrupteur actuellement eteint
+			if ($state==1)
+			{
+				$image = 'img/interrupteur-off.png';
+				$nextstate = '0';
+			}
+			// Gpio Low = Interrupteur actuellement allume
+			elseif($state==0)
+			{
+				$image = 'img/interrupteur-on.png';
+				$nextstate = '1';
+			}
+			// Gpio -1 = erreur ?
+			elseif($this->_gpioaccess->getValue()==-1)
+			{
+				$retour='Erreur : etat anormal du GPIO';
+				return $retour;
+			}
+				
+			$retour='
+				<div id="' . $iddiv . '">					
+					<img style="width:100%;" id="' . $idimg . '" src="' . $image . '"></img>
+						
+					<script type="text/javascript">
+					$(document).ready(function()
+						{
+				
+							$("#' . $idimg . '").on(\'click\', function(even) {
+								
+								$("#' . $iddiv . '").load(\'minuteur.php\',{ name:"' . $name . '", pin:' . $pin . ', label:"' . $label . '", state:' . $nextstate . '});
+							});			
+						});
+					</script>
+				</div>
+				';
+				
+		}
+		catch (GpioException $e)
+		{
+			throw new \GpioException('GPIO Exception => ' . $e);
+		}
+		catch (ShellException $e)
+		{
+			throw new \ShellException('Shell Exception => ' . $e);
+		}
+		
+		return $retour;
+		
+	}
+
+}
+
+
+// Détecte si requete post et bons arguments, auquel cas déclenche l'acces GPIO :
+if (isset($_POST['name']) and isset($_POST['pin']) and isset($_POST['state']) and isset($_POST['tempo']) and isset($_POST['label']))
+{
+
+	try
+	{
+
+		// Récupère les paramètres
+		$name = $_POST['name'];
+		$pin = $_POST['pin'];
+		$state = $_POST['state'];
+		$tempo = $_POST['tempo'];
+		$label = $_POST['label'];
+
+		// Instancie un Gpio
+		$minuteur = new Minuteur($name, $pin, $label);
+
+		// Modifie l'état du Gpio, avec comme délai 0 puisqu'il s'agit d'un objet de type Interrupteur
+		$minuteur->setState($state, $tempo);
+
+		// Affiche le nouvel interrupteur
+		echo $minuteur;
+
+	}
+	catch (Exception $e)
+	{
+		throw new \GpioException('Minuteur Exception => ' . $e);
+	}
+
+}
+
+?>
